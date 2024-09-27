4384b358fa923bf97274e97668e69a263eb7012c
j25ng <jsan2861@gmail.com>
2024-09-27 17:45:43 +0900

    0.3.0

--- src/stdash/app.py
+++ src/stdash/app.py
@@ -2,14 +2,11 @@ import streamlit as st
 
 st.title('CNN JOB MONITORING')
 
-rp_page = st.Page("pages/cnt_time.py", title="Time", icon=":material/dashboard:")
-cnt_page = st.Page("pages/cnt_user.py", title="User", icon=":material/dashboard:")
+rp_page = st.Page("pages/cnt_time.py", title="Count Time", icon="⏱️") 
+cnt_page = st.Page("pages/cnt_user.py", title="Count User", icon="😊")
+file_page = st.Page("pages/upload_file.py", title="Upload File", icon="🖼️")
 
-pg = st.navigation(
-        {
-            "Req-Prd" :[rp_page, cnt_page]
-        }
-)
+pg = st.navigation([rp_page, cnt_page, file_page])
 
 pg.run()
 

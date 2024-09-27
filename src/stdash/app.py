5259bc6ac08dae7ea6d7f8ad0e1ed8f2097d65e0
j25ng <jsan2861@gmail.com>
2024-09-27 15:57:18 +0900

    add pages

--- src/stdash/app.py
+++ src/stdash/app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
+st.title('CNN JOB MONITORING')
 
-rp_page = st.Page("cnt_time.py", title="Time", icon=":material/dashboard:")
-cnt_page = st.Page("cnt_user.py", title="User", icon=":material/dashboard:")
+rp_page = st.Page("pages/cnt_time.py", title="Time", icon=":material/dashboard:")
+cnt_page = st.Page("pages/cnt_user.py", title="User", icon=":material/dashboard:")
 
 pg = st.navigation(
         {

f084d7886d07bad4df74e599d9931d6c0843ef41
j25ng <jsan2861@gmail.com>
2024-10-02 12:50:07 +0900

    upload done

--- src/stdash/pages/upload_file.py
+++ src/stdash/pages/upload_file.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from rightnow.time import now
 import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
@@ -6,17 +7,21 @@ import requests
 
 st.title('Upload File')
 
-def load_data():
-    url = 'http://43.202.66.118:8016/all'
-    #url = 'http://127.0.0.1:8016/all'
-    r = requests.get(url).json()
+def upload_file(file):
+    url = "http://localhost:8016/uploadfile/" 
+    label = file.name.split('_')[0]
+    files = {"file": (file.name, file.getvalue(), file.type)}
+    data = {"label": int(label)}
+
+    r = requests.post(url, files=files, data=data)
     return r
 
-def send_file(file):
-    pass
+file = st.file_uploader('', type=['png', 'jpg', 'jpeg'])
 
-upload_file = st.file_uploader('이미지를 업로드 하세요.', type=['png', 'jpg', 'jpeg'])
+if file is not None:
+    st.write(Image.open(file))
+    if st.button("upload"):
+        r = upload_file(file)
+        st.write(r.json())
 
-if upload_file is not None:
-    st.write(Image.open(upload_file))
-    st.button("제출", on_click=send_file, args=[upload_file])
+
